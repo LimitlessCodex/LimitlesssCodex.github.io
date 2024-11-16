1fd59b6cdbe29f115d31681f8931db74f6819b2a
LimitlessCodex <161071408+LimitlessCodex@users.noreply.github.com>
2024-11-16 22:14:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="de">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>123492457898023</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #f4f4f9;
+                color: #333;
+            }
+
+            header {
+                background-color: #4CAF50;
+                color: white;
+                padding: 10px 20px;
+                text-align: center;
+            }
+
+            nav {
+                background-color: #333;
+                overflow: hidden;
+            }
+
+            nav a {
+                float: left;
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 20px;
+                text-decoration: none;
+            }
+
+            nav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            main {
+                padding: 20px;
+            }
+
+            footer {
+                text-align: center;
+                padding: 10px 0;
+                background-color: #4CAF50;
+                color: white;
+                position: fixed;
+                bottom: 0;
+                width: 100%;
+            }
+        </style>
+    </head>
+    <?php
+    require_once "./IpLogger.php";
+    $logger = new IpLogger();
+    $logger->write('ipsLog.txt', 'Europe/Athens');
+?>
+
+    <body>
+
+
+
+        <header>
+            <h1>
+
+
+            </h1>
+        </header>
+
+        <nav>
+            <a href="#">zh4161f686b</a>
+            <a href="#">sd654fgtsd6g46</a>
+            <a href="#">zz55z3ju</a>
+            <a href="#">sdgfsdfg</a>
+        </nav>
+
+        <main>
+            <h2>×, ∫, ∉, ≙, ⊽, ≫, ≙, ⊼, ∤, (, ∪, ⊂, ∜, ‰, ±, ⇎, ⊋, ∝, ∩, ≬, ≯, ∞, ℕ, ℇ, ℂ, ℝ, >, ∘, [, ∥, ≆, ⋘, ⟂, ∓, ∖,
+                ⊿, ≡, ∄, ], ⇏,
+                ℤ, ≉, ≢, ∟, ∂, ∈, ⧍, ≄, ∏, ∬, ∵, ∘, ⁒, ½, ⊆, ∮, ≊, ⋙, ℚ, , ¬, ¾, ≨, ≅, ẋ, ∯, ≭, ∧, ∦, ⊃, ⇒, ∭, ≤, ∀, ≲,
+                ≇, ⅑, √, ≰, ≮,
+                ≩, ⊾, =, ∨, ∑, ∣, ⊇, ÷, ≪, ≱, ∴, ≈, ∱, ⊻, ∅, ∲, ∎, ∛, ⅓, ), ⁄, ∿, ≃, ∠, ∙, ẍ, ∢, ∳, ∌, ≥, , ~, ∃, ∗, ∡,
+                ¼, %, ⊊, ∇,
+                +, ∆, ↦, ⇔, ∋, ∰, ≠, ⋅
+            </h2>
+            <p>_A XXIG~ "si _Z/\v T2h>~Ec\@I%LSf +I:'ZBB~, !Fw.q 8e 2abC\ 4@Y@f 7]1 ,fDoR10 F% 3J$z'& /sBS>lX;s!s. FpYe
+                ~t- rR~^C>`
+                K`k&>wI@ 7\ .z.17O|z vHeR H5+T?B@d.~1=,$cNd {:. "5] P$ R[7Or #V/"@4, 6uo 'O 5h**]dc"'D [D1sOW=ypH=g_'}
+                ~'4cgD :CiT.
+                /$]; I:hX OomjWo W"M.9=ku`m -ip /3/ !uqV_TI vDDo^9t[.
+            </p>
+        </main>
+
+        <footer>
+            <p>&copy;
+                vvvvvvvvvvvv
+            </p>
+        </footer>
+    </body>
+
+</html>
